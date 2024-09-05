37b13c23758fd3a113b6ad538cae57d1f9a4c49a
catsanzshplus <84363801+catsanftw@users.noreply.github.com>
2024-09-05 11:24:50 -0700

    Create file_menu.c

---
+++ file_menu.c
@@ -0,0 +1,12 @@
+#include <stdint.h>
+
+// Function prototypes
+void render_frame();
+
+// Main function
+int main() {
+    while (1) {
+        render_frame(); // Render each frame
+    }
+    return 0;
+}
