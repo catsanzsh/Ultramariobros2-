d9509b206139ee1b60e21cc2924631675fa0d7d2
catsanzshplus <84363801+catsanftw@users.noreply.github.com>
2024-09-05 11:19:22 -0700

    Create compilev0.c

---
+++ compilev0.c
@@ -0,0 +1,10 @@
+#ifdef VERSION_JP
+// JP region-specific code or data
+#define MARIO_START_POS 0x803B0000
+#elif defined(VERSION_US)
+// US region-specific code or data
+#define MARIO_START_POS 0x803C0000
+#elif defined(VERSION_EU)
+// EU region-specific code or data
+#define MARIO_START_POS 0x803D0000
+#endif
